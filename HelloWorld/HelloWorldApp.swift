3a84fdcbfa4b3c92912d219371aa6f444033b73b
Michael Rowsey <michael@rowsey.org>
2026-05-29 13:39:12 -0700

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Michael Rowsey on 5/29/26.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
